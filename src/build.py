a34a4de47420022f98f26ebc724a4df1e29686bf
TSA Developer <tsa@host-41.dmz2.mfn.local>
2021-10-27 12:17:30 +0200

    Add version check to build script

--- src/build.py
+++ src/build.py
@@ -1,5 +1,9 @@
 from tools.build.main import main
+import sys
 
+version = sys.version_info[0]+sys.version_info[1]*0.1
+if(version < 3.9):
+    raise Exception("Must be using Python >=3.9")
 if __name__ == "__main__":
     # Ask user an create build folder with contents
     main()
